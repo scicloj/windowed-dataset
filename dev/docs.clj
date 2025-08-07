cd37d7b53a50429b0a045cbce8f18217a7973a32
daslu <daniel.slutsky@gmail.com>
2025-08-08 01:10:23 +0300

    deps and clay setup

---
+++ dev/docs.clj
@@ -0,0 +1,10 @@
+(ns docs
+  (:require [scicloj.clay.v2.api :as clay]))
+
+(clay/make! {:format [:quarto :html]
+             :base-source-path "notebooks"
+             :source-path ["index.clj"
+                           "windowed_dataset_examples.clj"]
+             :base-target-path "docs"
+             :book {:title "Windowed dataset"}
+             :clean-up-target-dir true})
